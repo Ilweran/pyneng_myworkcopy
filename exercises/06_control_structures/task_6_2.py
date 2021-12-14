ea3e9dbca18187f99878995f994536159045bc43
Cloud Test <cloud_test@example.com>
2021-12-14 12:32:57 -0500

    Tasks 6-1 and 6-2 done.

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,15 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_address  = input("Input IP address in the format X.X.X.X: ")
+if (int(ip_address.split(".")[0]) >= 1) and (int(ip_address.split(".")[0]) <= 223):
+    print("unicast")
+elif (int(ip_address.split(".")[0]) >= 224) and (int(ip_address.split(".")[0]) <= 239):
+    print("multicast")
+elif (int(ip_address.split(".")[0]) == 255) and (int(ip_address.split(".")[1]) == 255) and (int(ip_address.split(".")[2]) == 255) and (int(ip_address.split(".")[3]) == 255):
+    print("local broadcast")
+elif (int(ip_address.split(".")[0]) == 0) and (int(ip_address.split(".")[1]) == 0) and (int(ip_address.split(".")[2]) == 0) and (int(ip_address.split(".")[3]) == 0):
+    print("unassigned")
+else:
+    print("unused")
